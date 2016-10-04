54e94315f58141f8e552d22bad54782f4a6b310d
ftd397 <trilochan.poudel@capitalone.com>
2016-10-03 20:38:49 -0700

    added gif

---
+++ Carousel/LogOutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LogOutViewController.swift
+//  Carousel
+//
+//  Created by Poudel, Trilochan on 10/3/16.
+//  Copyright © 2016 Stars99. All rights reserved.
+//
+
+import UIKit
+
+class LogOutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func didPressLogOutButton(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
